de49d7d29cab53859c4081d583502686f75f3d65
DoKep <86494191+DoKep2@users.noreply.github.com>
2022-11-21 09:24:23 -0800

    qweqwe

---
+++ WebApplication4/WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Net;
+
+namespace WebApplication4
+{
+   
+            public partial class WebForm1 : System.Web.UI.Page
+            {
+                protected void Page_Load(object sender, EventArgs e)
+                {
+
+                }
+            }
+        }
